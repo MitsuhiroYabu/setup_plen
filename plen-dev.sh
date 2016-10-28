c51f9b44f89e3a2e66051d31c51ec05968fcbdf7
mitsuhiro <mitsuhirooooo@gmail.com>
2016-10-28 17:13:35 +0900

    hostapd launch

--- plen-dev.sh
+++ plen-dev.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+tmp=$(systemctl status hostapd | grep Active | tr -d " ")
+
+if [ $tmp = "Active:inactive(dead)" ]
+then
+	echo hostapd is not running
+	systemctl stop wpa_supplicant
+	systemctl disable wpa_supplicant
+	systemctl enable hostapd
+	systemctl start hostapd
+else
+	echo hostapd is running
+fi
+
 rfkill unblock bluetooth
 source /etc/profile
 roslaunch plen plen.launch
 
-while true
-do
-done
